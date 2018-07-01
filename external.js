143ad51b113fa5ac7fcc6d708fa3b5154f8b0e21
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2018-07-01 12:24:30 -0300

    Add files via upload
    Primeiras versoes da aula..vou refazer os códigos depois

---
+++ external.js
@@ -0,0 +1,8 @@
+
+/*my name is kamila
+*/
+
+var a=70; //decalring and initializaing variable A
+var b=60; //DECALRING AND initializaing variable b wiht 60
+var sum= a+b; //adding value of a and b variable in the variable sum
+document.write(sum); //printing the value of sum variable the screen
